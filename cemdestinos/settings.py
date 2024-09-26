b7ac579fe99edb06492d4723314797418552076c
004charles <muquissicarlos@gmail.com>
2024-09-26 09:12:15 -0300

    Descrição

--- cemdestinos/settings.py
+++ cemdestinos/settings.py
@@ -14,7 +14,8 @@ SECRET_KEY = 'django-insecure-2e1j#dh9xjotm9f8042!px+0_xc4!y*c#d#ii40p0z3j1d(@5=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
+
 
 
 # Application definition
